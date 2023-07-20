101b56dc7359b64ac9ff28ab05fb115a6a793232
BigDataMao <mxw712@gmail.com>
2023-07-20 23:33:52 +0800

    socket流lambda表达式优化写法

--- src/main/java/cn/chatdoge/wc/WordCountStreamingSocketLambda.java
+++ src/main/java/cn/chatdoge/wc/WordCountStreamingSocketLambda.java
@@ -1,12 +1,12 @@
 package cn.chatdoge.wc;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 
 public class WordCountStreamingSocketLambda {
@@ -18,16 +18,19 @@ public class WordCountStreamingSocketLambda {
 
         // TODO 1. 拆分单词
         SingleOutputStreamOperator<Tuple2<String, Integer>> wordAddOne;
-        wordAddOne = socketDS.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (s, collector) -> {
-            for (String word : s.split(" ")) {
-                collector.collect(Tuple2.of(word, 1));
-            }
-        });
+
+        wordAddOne = socketDS.flatMap(
+                (String s, Collector<Tuple2<String,Integer>> collector)
+                        ->
+                {for (String word : s.split(" ")) {
+                    collector.collect(Tuple2.of(word, 1));
+                }
+        }).returns(Types.TUPLE(Types.STRING,Types.INT)) // 必须显式返回flink流能识别的类型
+        ;
 
         // TODO 2. 分组
         KeyedStream<Tuple2<String, Integer>, String> keyedStream;
-        keyedStream = wordAddOne.keyBy(
-                (KeySelector<Tuple2<String, Integer>, String>) value -> value.f0);
+        keyedStream = wordAddOne.keyBy(value -> value.f0);
 
         // TODO 3. 求和
         SingleOutputStreamOperator<Tuple2<String, Integer>> sum = keyedStream.sum(1);
